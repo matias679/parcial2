02992af05e21add41c44625a373caa2df4956d8c
matias679 <mat.manzano@duocuc.cl>
2026-04-15 22:46:47 -0400

    Add files via upload

---
+++ tarea 1.py
@@ -0,0 +1,7 @@
+usuario = input("ingrese su usuario ")
+clave = input("ingrese su clave ")
+
+if usuario == "alumno" and clave == "estudiante21":
+    print ("usuario correcto")
+else:
+   print ("usuario o clave incorrectos", usuario, clave)
